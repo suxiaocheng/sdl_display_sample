9f88152ac87a72f747c5c5bf0b07520066bc9a63
suxiaocheng <Xiaocheng.Su@desay-svautomotive.com>
2019-08-17 17:16:45 +0800

    fix some compile error

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 CC      = gcc
 EXE     =
-CFLAGS  = -g -O2 -I/usr/local/include/SDL2 -DHAVE_OPENGLES -DHAVE_OPENGLES2 -DHAVE_OPENGL -I/home/V01.NET/uidq2055/program/jpeg/x86/jpeg-6b/install/include
+CFLAGS  = -g -O2 -I/usr/local/include/SDL2 -DHAVE_OPENGLES -DHAVE_OPENGLES2 -DHAVE_OPENGL 
 LIBS    =  -lSDL2 -lSDL2_test -L/usr/local/lib/ -ljpeg -L/usr/local/lib
 
 hello:main.c
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <unistd.h>
 #include "jpeglib.h"
 SDL_Window *g_pWindow = 0;
 SDL_Renderer *g_pRenderer = 0;
@@ -35,11 +36,16 @@ int main(int argc, char *args[])
 		JSAMPARRAY buffer;
 		int row_stride;
 		const char *filename="color_bar.jpeg";//"test.jpg";
+		unsigned char *file_buf;
+		unsigned long file_size = 0;
+		unsigned long file_size_read = 0;
+		unsigned char *display_buf;
 
 		if ((infile = fopen(filename, "rb")) == NULL) {
 			fprintf(stderr, "can't open %s\n", filename);
 			exit(1);
 		}
+
 		cinfo.err = jpeg_std_error(&jerr);
 		//jerr.pub.error_exit = my_error_exit;
 		/*if (setjmp(jerr.setjmp_buffer)) {
@@ -49,13 +55,32 @@ int main(int argc, char *args[])
 		}*/
 		jpeg_create_decompress(&cinfo);
 
+#if 1
 		jpeg_stdio_src(&cinfo, infile);
-		jpeg_read_header(&cinfo, TRUE);
-		jpeg_start_decompress(&cinfo);
+#else
+		fseek(infile, 0, SEEK_END);
+		file_size = ftell(infile);
+		printf("Jpeg file size: %ld\n", file_size);
+		file_buf = (unsigned char *)malloc(file_size);
+		if (file_buf == NULL) {
+			printf("file_buf is null\n");
+		}
+		fseek(infile, 0, SEEK_SET);
+		file_size_read = fread(file_buf, file_size, 1, infile);
+		if(file_size_read != 1) {
+			 printf("file_buf is not equal, %ld != %ld\n", 
+					 file_size_read, file_size);
+		}
+		jpeg_mem_src(&cinfo, file_buf, file_size);
+#endif
+		(void) jpeg_read_header(&cinfo, TRUE);
+		// cinfo.out_color_space=JCS_RGB;
+		(void) jpeg_start_decompress(&cinfo);
 
 		row_stride = cinfo.output_width * cinfo.output_components;
-		printf("r: %d, w: %d, ppb: %d\n", row_stride, cinfo.output_width,
-				cinfo.output_components);
+		printf("r: %d, w: %d, ppb: %d, type: %d\n", row_stride, cinfo.output_width,
+				cinfo.output_components, cinfo.out_color_space);
+		//sleep(1);
 		buffer = (*cinfo.mem->alloc_sarray)
 			((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
 
@@ -67,21 +92,24 @@ int main(int argc, char *args[])
 			int Gmask = 0x0000FF00;
 			int Bmask = 0x000000FF;
 			int i;
+			int ret;
 			unsigned char *pbuf;
 			unsigned int *pp;
 			pTempSurface = SDL_CreateRGBSurface(0, cinfo.output_width,
 				       cinfo.output_height, 32, 0, 0, 0, 0);
 			pp = pTempSurface->pixels;
 			while (cinfo.output_scanline < cinfo.output_height) {
-				jpeg_read_scanlines(&cinfo, buffer, 1);//TODO
+				(void) jpeg_read_scanlines(&cinfo, buffer, 1);//TODO
 				pbuf = buffer;
+				pp = pTempSurface->pixels + (cinfo.output_scanline - 1) * cinfo.output_width * 4;
 				for(i=0; i<cinfo.output_width; i++) {
-#if 1
+#if 0
 					*pp = ((unsigned int)pbuf[0]<<16) | 
 						((unsigned int)(pbuf[1])<<8) | 
 						((unsigned int)(pbuf[2])<<0) |
-						((unsigned int)0 <<24);
+						((unsigned int)0xff <<24);
 #else
+#if 0
 					if(i<cinfo.output_width/3){
 						*pp = 0x000000ff;
 					} else if(i<cinfo.output_width/3*2){
@@ -89,16 +117,22 @@ int main(int argc, char *args[])
 					} else {
 						*pp = 0x00ff0000;
 					}
+#else
+					unsigned char val = i*255/cinfo.output_width;
+					*pp = (val<<16) | (val<<8) | val;
+#endif
 #endif
 					pp++;
 					pbuf+=3;
 				}
 				// put_scanline_someplace(buffer[0], row_stride);
 			}
+			printf("last output line is: %d\n", cinfo.output_scanline);
 		}
-		jpeg_finish_decompress(&cinfo);
+		(void) jpeg_finish_decompress(&cinfo);
 
 		jpeg_destroy_decompress(&cinfo);
+		free(file_buf);
 		fclose(infile);
 	}
 #else
